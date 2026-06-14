8a6d313ca28bcb0a8b4ce1d4804685928959921d
tornierm <80321344+Tornierm@users.noreply.github.com>
2026-06-14 17:37:38 +0200

    Fix styled-components registry for client rendering

--- app/registry.tsx
+++ app/registry.tsx
@@ -1,18 +1,1 @@
-'use client';
-
-import React, { ReactNode } from 'react';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
-
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const sheet = React.useRef(() => new ServerStyleSheet());
-
-  return (
-    <StyleSheetManager sheet={sheet.current().instance}>
-      {children}
-    </StyleSheetManager>
-  );
-}
+'use client';\n\nimport React, { ReactNode } from 'react';\n\nexport default function StyledComponentsRegistry({\n  children,\n}: {\n  children: ReactNode;\n}) {\n  return <>{children}</>;\n}\n
